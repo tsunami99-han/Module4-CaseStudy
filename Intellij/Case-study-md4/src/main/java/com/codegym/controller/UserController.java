a5aba4d61217f2d3062b0a182a10a89c7d343714
thaonoucamp <dangxuanthaoht@gmail.com>
2021-08-06 10:58:27 +0700

    find by name

--- Intellij/Case-study-md4/src/main/java/com/codegym/controller/UserController.java
+++ Intellij/Case-study-md4/src/main/java/com/codegym/controller/UserController.java
@@ -1,11 +1,33 @@
 package com.codegym.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codegym.model.User;
+import com.codegym.service.user.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("users")
 @CrossOrigin("*")
 public class UserController {
+    @Autowired
+    private IUserService userService;
+
+    @GetMapping("/search/{username}")
+    public ResponseEntity<List<User>> findByUserName(@PathVariable("username") String username) {
+        Optional<User> userList = userService.findByUsername(username);
+        return new ResponseEntity(userList, HttpStatus.OK);
+    }
+
+//    @GetMapping("/search/")
+//    public ResponseEntity<List<User>> findAllFriendCommon(){
+//        Optional<User> friendsCommon = userService.findByUsername();
+//
+//        return new ResponseEntity(friendsCommon ,HttpStatus.OK);
+//    }
 }
